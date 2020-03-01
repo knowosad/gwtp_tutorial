dc073ae8ddfc5d55b10d5a437f2d5e63baae8658
Unknown <k.nowosad@hotmail.com>
2020-03-01 17:07:35 +0100

    Added event handler

---
+++ gwtp-begginer/src/main/java/pl/knowosad/client/application/home/HomeUiHandlers.java
@@ -0,0 +1,8 @@
+package pl.knowosad.client.application.home;
+
+import com.gwtplatform.mvp.client.UiHandlers;
+
+public interface HomeUiHandlers extends UiHandlers {
+
+  void onSend(String name);
+}
